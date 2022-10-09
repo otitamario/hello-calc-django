fb111ef95de5c5b42f0ea288eb47e8fd00d2ec56
Mario S J <mario2013sj@gmail.com>
2022-10-09 00:47:04 -0400

    Start project

---
+++ core/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def hello(request):
+    return HttpResponse('Hello World')
+
+def sum(request,a,b):
+    op=a+b
+    return HttpResponse('<h1>The sum of  {0} and {1} is {2}</h1>'.format(a,b,op))
+
+def sub(request,a,b):
+    op=a-b
+    return HttpResponse('<h1>The difference of  {0} and {1} is {2}</h1>'.format(a,b,op))
+
+def mult(request,a,b):
+    op=a*b
+    return HttpResponse('<h1>The multiplication of  {0} and {1} is {2}</h1>'.format(a,b,op))
+
+def div(request,a,b):
+    op=a/b
+    if b!=0:
+        return HttpResponse('<h1>The division of  {0} by {1} is {2}</h1>'.format(a,b,op))
+    else:
+        return HttpResponse('<h1>Cannot divide the numbers because the divisor is 0</h1>')
